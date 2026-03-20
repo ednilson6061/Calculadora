41605b637295334e7e64bbfa5d8ebb21b8303249
ednilson6061 <ednilsonrodrigues2004@gmail.com>
2026-03-20 16:00:11 -0300

    update calculadora

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.example.calculadora
-
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+    private lateinit var txtResultado: TextView
+    private var valorAtual = ""
+    private var numero1 = 0.0
+    private var operacao = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        txtResultado = findViewById(R.id.txtResultado)
+    }
+
+    fun clicarNumero(view: View) {
+        val button = view as Button
+        valorAtual += button.text
+        txtResultado.text = valorAtual
+
+    }
+    fun clicarOperacao(view: View) {
+        val botao = view as Button
+        operacao = botao.text.toString()
+        numero1 = valorAtual.toDouble()
+        valorAtual = ""
     }
 }
